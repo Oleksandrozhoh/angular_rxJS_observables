0a3af5e321c35473f6396f025fb6033b32d6b21f
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2025-06-20 16:02:08 -0500

    example of observable and signal event tracking, also example of observalbe interval()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    // Create an interval observable that emits a value every second (app might need to send http requests or perform other tasks periodically)
+    const subscription = interval(1000).pipe(
+      map(value => value *2) // RxJS operator map function to transform the emitted value
+    ).subscribe({
+      next: (value) => {
+        console.log('Interval value:', value);
+      },
+      // error: (err) => {
+      //   console.error('Error occurred:', err);
+      // },
+      // complete: () => {
+      //   console.log('Interval completed');
+      // }
+    });
+
+    this .destroyRef.onDestroy(() => {
+      subscription.unsubscribe(); // Unsubscribe when the component is destroyed (important for memory management)
+    }
+    );
+  }
+
+  private clickCount = signal(0); // Create a signal to hold the click count
+
+  private $clickCount = toObservable(this.clickCount); // Convert the signal to an observable
+
+  constructor() {
+    // Subscribe to the observable to react to changes in the click count
+    this.$clickCount.subscribe({
+      next: (value) => {
+        console.log('OBSERVABLE: Click count:', value); // Log the updated click count
+      }
+    });
+
+    // Effect to react to changes in the click count signal value
+    effect(() => {
+      console.log('EFFECT: Click count:', this.clickCount()); // Log the click count whenever it changes
+    })
+
+  }
+
+  onClick() {
+    this.clickCount.update(value => value + 1); // Increment the click count
+  }
+
 
 }
